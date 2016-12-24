75cb4af8d11e1140e499ea5f401bee526338370a
PawelStepak <pawel.stepak@mobilevikings.pl>
2016-12-25 00:06:01 +0100

    cleaning up utils

--- src/mocker/utils.py
+++ src/mocker/utils.py
@@ -45,44 +45,51 @@ def make_callback(hashed_id, data):
         return callback.status_code
 
 
+def perform_http_request(url, requested_http_method, destination_header):
+    r = None
+    if requested_http_method == "GET":
+        r = requests.get(url, headers=destination_header)
+    elif requested_http_method == "POST":
+        r = requests.post(url, headers=destination_header)
+    elif requested_http_method == "PATCH":
+        r = requests.patch(url, headers=destination_header)
+    elif requested_http_method == "PUT":
+        r = requests.put(url, headers=destination_header)
+    return r
+
+
 def process_request(hashed_id,
                     requested_http_method,
                     requested_content_type,
                     absolute_uri,
                     forced_format):
     """
-    Perfornming HTTP operations on mocked API
+    Performing HTTP operations on mocked API
     """
     mock = Mocker.objects.get(hashed_id=hashed_id)
     
     original_destination_address = mock.original_destination_address
+
     callback_api = mock.callback_address
 
-    allowed_mocked_allowed_http_method = mock.mocked_allowed_http_method
-    allowed_content_type = mock.mocked_allowed_content_type
+    mocked_allowed_http_method = mock.mocked_allowed_http_method
+    mocked_allowed_content_type = mock.mocked_allowed_content_type
 
     url = absolute_uri
     params = url[url.find(hashed_id)+len(hashed_id)+1:]
 
-    # check if http method is allowed
-    if requested_http_method == allowed_mocked_allowed_http_method:
-        # check if content_type is allowed
-        if requested_content_type == str(allowed_content_type):
+    # check if requested http method is allowed
+    if requested_http_method == mocked_allowed_http_method:
+        # check if requested content_type is allowed
+        if requested_content_type == str(mocked_allowed_content_type):
             url = ''.join([original_destination_address, params])
 
             if requested_content_type == 'application/json' or forced_format == "json":
                 destination_header = {'Content-type': str(requested_content_type)}
-                if requested_http_method == "GET":
-                    r = requests.get(url, headers=destination_header)
-                elif requested_http_method == "POST":
-                    r = requests.post(url, headers=destination_header)
-                elif requested_http_method == "PATCH":
-                    r = requests.patch(url, headers=destination_header)            
-                elif requested_http_method == "PUT":
-                    r = requests.put(url, headers=destination_header)  
-                else:
-                    return JsonResponse({"status": "Something went wrong"}, status=500)
-                    
+                r = perform_http_request(url, requested_http_method, destination_header)
+                if not r:
+                    return JsonResponse({"status": "Not recognized HTTP method"}, status=500)
+
                 if r.status_code == requests.codes.ok:
                     if callback_api:
                         response = make_callback(hashed_id, data=r)
@@ -97,9 +104,9 @@ def process_request(hashed_id,
                         status_code = 500
                     return JsonResponse(json.dumps(data), safe=False, status=status_code)
                 else:
-                    return JsonResponse({"status": "Something went wrong"}, status=r.status_code)
+                    return JsonResponse({"status": "ERROR: HTTP signal has broken"}, status=r.status_code)
         else:
-            return JsonResponse({"status": "Content type: text/plain is not allowed"}, status=405)
+            return JsonResponse({"status": "Requested Content type is not allowed"}, status=405)
     else:
         return JsonResponse({"status": "Requested HTTP method is not allowed"}, status=405)
 
